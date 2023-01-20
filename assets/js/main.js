65ab1928ef4b1dd3042be7cb996688ffa9f6735a
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-01-20 21:16:32 +0800

    responsive menu

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// TOGGLE MENU
+
+const navToggle = document.getElementById("nav-toggle"),
+    navMenu = document.getElementById("nav-menu");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("top-[-1000%]");
+    navMenu.classList.toggle("top-10");
+});
